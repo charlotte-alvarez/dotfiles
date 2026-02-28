a3166e16f118888f70ab28927e20c7297828e26b
Charlotte <email@armandalvarez.com>
2026-02-27 22:47:14 -0500

    fixed keymap descriptions and compressed basic keymaps to one block

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,10 +1,6 @@
 local k = vim.keymap
 
-
-k.set('i', 'jk', '<Esc>', { desc = 'exit insert with jk'})
-
-k.set('n', '<leader>d', '"_d' { desc = 'delete without removing clipboard'})
-k.set('v', '<leader>d', '"_d' { desc = 'delete without removing clipboard'})
-
-k.set('n', '<Esc>', '<cmd>nohlsearch<CR>' { desc = 'Remove highlighting when esc pressed'})
-
+k.set('i', 'jk', '<Esc>', { desc = 'exit insert with jk' })
+k.set('n', '<leader>d', '"_d', { desc = 'delete without removing clipboard' })
+k.set('v', '<leader>d', '"_d', { desc = 'delete without removing clipboard' })
+k.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Remove highlighting when esc pressed' })
