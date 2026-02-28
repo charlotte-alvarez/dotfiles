a49421239e95db6c752d2661af4cc3e90886b7d1
Charlotte <email@armandalvarez.com>
2026-02-28 14:38:35 -0500

    Formatting

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,11 +1,10 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  opts = {
-
-  }
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+    }
 }
