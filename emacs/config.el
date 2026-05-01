352bd468251669c2660489b6f7b9cf812882b8da
charlotte-alvarez <email@armandalvarez.com>
2026-05-01 14:21:17 -0400

    Added which-key, and gui/font improvements

--- emacs/config.el
+++ emacs/config.el
@@ -95,13 +95,15 @@
     "ff" '(find-file :wk "Find file")
 
     "foc" '(lambda () (interactive)
-             (find-file char/emacs-config-org))
+             (find-file char/emacs-config-org) :wk "Open emacs config")
 
     ;; Help keys
     "h" '(:ignore t :wk "help")
     "hk" '(describe-key :wk "Describe key")
     "hc" '(describe-key-briefly :wk "Describe key briefly")
-  )
+    "hx" '(describe-command :wk "Describe command")
+    "hf" '(describe-function :wk "Describe function")
+    )
 
   ;; Reload config (keep inside :config!)
   (defun char/reload-config ()
@@ -111,3 +113,53 @@
 
   (char/leader-keys
     "fr" '(char/reload-config :wk "Reload config")))
+
+(set-face-attribute 'default nil
+		    :font "JetBrains Mono"
+		    :height 110
+		    :weight 'medium)
+(set-face-attribute 'variable-pitch nil
+		    :font "JetBrains Mono"
+		    :height 120
+		    :weight 'medium)
+(set-face-attribute 'fixed-pitch nil
+		    :font "JetBrains Mono"
+		    :height 110
+		    :weight 'medium)
+;; Makes commented text italicized (is that a real word?).
+;; Your font must have an italic face available.
+(set-face-attribute 'font-lock-comment-face nil
+		    :slant 'italic)
+
+;; This sets the default font on all graphical frames created after restarting Emacs.
+;; Does the same thing as 'set-face-attribute default' above, but emacsclient fonts
+;; are not right unless I also add this method of setting the default font.
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-11"))
+
+;; Uncomment the following line if line spacing needs adjusting.
+(setq-default line-spacing 0.12)
+
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+(global-visual-line-mode t)
+
+(use-package which-key
+  :init
+    (which-key-mode 1)
+  :config
+  (setq which-key-side-window-location 'bottom
+	  which-key-sort-order #'which-key-key-order-alpha
+	  which-key-sort-uppercase-first nil
+	  which-key-add-column-padding 1
+	  which-key-max-display-columns nil
+	  which-key-min-display-lines 6
+	  which-key-side-window-slot -10
+	  which-key-side-window-max-height 0.25
+	  which-key-idle-delay 0.8
+	  which-key-max-description-length 25
+	  which-key-allow-imprecise-window-fit t
+	  which-key-separator " → " ))
