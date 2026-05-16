4026b50700cd79e29c25a58c655824449ac2456a
charlotte-alvarez <email@armandalvarez.com>
2026-05-16 11:17:38 -0400

    feat(early-init): Removed menu bar tool bar and scroll bar. Inhibited splash and startup screen

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,1 +1,11 @@
+;; Disable package.el at startup, since we are using Elpaca
 (setq package-enable-at-startup nil)
+
+;; Inhibit splash & startup screens
+(setq inhibit-splash-screen t
+      inhibit-startup-screen t)
+
+;; Disable menubar, toolbar, scrollbar
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
